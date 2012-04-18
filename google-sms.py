3ea96590ad8c478e85893a835ea507014a1b0348
Safronov Pavel <pv.safronov@gmail.com>
2012-04-19 02:40:26 +0400

    PEP8-compatible. Removed double definition of function "usage" added by mistake

--- google-sms.py
+++ google-sms.py
@@ -9,29 +9,35 @@ import getopt
 import sys
 import time
 version = "0.01"
-config = {  'email':'mail@example.com',
-            'password':'passwd',
-            'calendar':'default',
-            'title':'Title of your event here',
-            'location':'Location of your event here'
+config = {'email':'mail@example.com',
+        'password':'passwd',
+        'calendar':'default',
+        'title':'Title of your event here',
+        'location':'Location of your event here'
 }
+
+
 def usage():
     """Prints right usage for this script"""
-    print("Usage: "+sys.argv[0]+" [-t title] [-l location] [-e email] [-p password]\n\r\
+    print("Usage: " + sys.argv[0] + " [-t title] [-l location] \
+                                        [-e email] [-p password]\n\r\
     -e  --email=EMAIL           set your email for Google account\n\r\
     -p  --password=PASSWORD     set password for your Google account\n\r\
     -t  --title=TITLE           set title of event\n\r\
     -l  --location=LOCATION     set location of event\n\r\
-    -c  --calendar=CALENDAR     set calendar name (default if not specifyed\n\r\
+    -c  --calendar=CALENDAR     set calendar name \
+                                (default if not specifyed)\n\r\
     -h  --help                  display this help and exit\n\r\
     -v  --version               display script version and exit")
+
+
 class SMS():
-    def __init__(self,email,password,title,location,calendar):
+    def __init__(self, email, password, title, location, calendar):
         """Connect to Google Calendar"""
         self.title = title
         self.location = location
         self.calendar_title = calendar
-        self.calendar_link = "http://www.google.com/calendar/feeds/default/private/full"
+        self.calendar_link = "/calendar/feeds/default/private/full"
         self.calendar_service = gdata.calendar.service.CalendarService()
         self.calendar_service.email = email
         self.calendar_service.password = password
@@ -49,6 +55,7 @@ class SMS():
         for a_calendar in self.feed.entry:
             if self.calendar_title == a_calendar.title.text:
                 self.calendar_link = a_calendar.link[0].href
+
     def send(self):
         """Add new event and set SMS-reminder"""
         event = gdata.calendar.CalendarEventEntry()
@@ -56,34 +63,33 @@ class SMS():
         event.content = atom.Content(text='')
         event.where.append(gdata.calendar.Where(value_string=self.location))
         # Set start time in 6 minutes
-        start_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime(time.time() + 6*60))
+        start_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z',
+                                    time.gmtime(time.time() + 6 * 60))
         # Set end time in an hour
-        end_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime(time.time() + 3600))
-        event.when.append(gdata.calendar.When(start_time=start_time, end_time=end_time))
-        minutes=5
+        end_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z',
+                                    time.gmtime(time.time() + 3600))
+        event.when.append(gdata.calendar.When(start_time=start_time,
+                                                end_time=end_time))
+        minutes = 5
         for a_when in event.when:
             if len(a_when.reminder) > 0:
                 # Adding reminder in 5 minutes before event (start_time)
                 a_when.reminder[0].minutes = 5
             else:
-                a_when.reminder.append(gdata.calendar.Reminder(minutes=minutes))
+                a_when.reminder.append(
+                        gdata.calendar.Reminder(minutes=minutes))
         # Insert new event
-        new_event = self.calendar_service.InsertEvent(event, self.calendar_link)
+        new_event = self.calendar_service.InsertEvent(event,
+                                                    self.calendar_link)
         return new_event
-def usage():
-    """Prints right usage for this script"""
-    print("Usage: "+sys.argv[0]+" [-t title] [-l location] [-e email] [-p password]\n\r\
-    -e  --email=EMAIL           set your email for Google account\n\r\
-    -p  --password=PASSWORD     set password for your Google account\n\r\
-    -t  --title=TITLE           set title of event\n\r\
-    -l  --location=LOCATION     set location of event\n\r\
-    -c  --calendar=CALENDAR     set calendar name to which you want to add event\n\r\
-    -h  --help                  display this help and exit\n\r\
-    -v  --version               display script version and exit")
+
+
 def main():
     """Check args, setting values and try to send SMS"""
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hve:p:t:l:c:", ["help", "email=", "password=", "title=","location=","calendar="])
+        opts, args = getopt.getopt(sys.argv[1:], "hve:p:t:l:c:",
+                                    ["help", "email=", "password=",
+                                    "title=", "location=", "calendar="])
     except getopt.GetoptError as err:
         """ print help information and exit """
         print(err)
@@ -94,25 +100,26 @@ def main():
     title = config['title']
     location = config['location']
     calendar = config['calendar']
-    for o,a in opts:
+    for o, a in opts:
         if o == "-v":
-            print("Python script for sending free SMS via Google Calendar v"+version)
+            print("Python script for sending free SMS \
+                    via Google Calendar v" + version)
             sys.exit()
-        elif o in ("-h","--help"):
+        elif o in ("-h", "--help"):
             usage()
             sys.exit()
-        elif o in ("-e","--email"):
+        elif o in ("-e", "--email"):
             email = a
-        elif o in ("-p","--password"):
+        elif o in ("-p", "--password"):
             password = a
-        elif o in ("-t","--title"):
+        elif o in ("-t", "--title"):
             title = a
-        elif o in ("-l","--location"):
+        elif o in ("-l", "--location"):
             location = a
-        elif o in ("-c","--calendar"):
+        elif o in ("-c", "--calendar"):
             calendar = a
     try:
-        sms = SMS(email,password,title,location,calendar)
+        sms = SMS(email, password, title, location, calendar)
     except:
         raise
     else:
